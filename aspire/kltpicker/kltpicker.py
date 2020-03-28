ca4c164baca773ee0bad4c4ef861c950b2cbec75
dalitcohen <dalitco54@gmail.com>
2020-03-28 23:32:20 +0300

    added figures

--- aspire/kltpicker/kltpicker.py
+++ aspire/kltpicker/kltpicker.py
@@ -289,6 +289,7 @@ class KLTPicker:
         self.num_of_particles = -1
         self.num_of_noise_images = 0
         self.threshold = 0
+        self.show_figures = 1
         patch_size = np.floor(0.8 * self.mgscale * self.particle_size)  # need to put the 0.8 somewhere else.
         if np.mod(patch_size, 2) == 0:
             patch_size -= 1
@@ -784,24 +785,50 @@ def main():
     #print("Starting preprocessing.")
     # kltpicker.preprocess()
     get_matlab_files_preprocess(kltpicker)
-    #print("Preprocess finished.\nFetching micrographs:")
+    print("Preprocess finished.\nFetching micrographs:")
     kltpicker.get_micrographs()
     print("Fetched micrographs.\nCutoff filter:")
     for micrograph in kltpicker.micrographs:
-        #micrograph.cutoff_filter(kltpicker.patch_size)
-        #print("Done cutoff filter")
-        #micrograph.estimate_rpsd(kltpicker.patch_size, kltpicker.max_iter)
-        #print("Done estimate rpsd I")
-        #micrograph.approx_noise_psd = micrograph.approx_noise_psd + np.median(micrograph.approx_noise_psd)/10
-        #micrograph.prewhiten_micrograph()
-        #print("Done prewhitening")
-        #micrograph.estimate_rpsd(kltpicker.patch_size, kltpicker.max_iter)
-        #print("Done estimate rpsd II")
-        #micrograph.psd = np.abs(trig_interpolation(np.pi * micrograph.r.astype('float64'), micrograph.approx_clean_psd, kltpicker.rho.astype('float64')))
+        micrograph.cutoff_filter(kltpicker.patch_size)
+        print("Done cutoff filter.\nEstimating RPSD I:")
+        micrograph.estimate_rpsd(kltpicker.patch_size, kltpicker.max_iter)
+        print("Done estimating RPSD I.")
+        if kltpicker.show_figures:
+            plt.figure(1)
+            plt.plot(micrograph.r*np.pi, micrograph.approx_clean_psd, label = 'Approx Clean PSD')
+            plt.title('Approx Clean PSD stage I')
+            plt.legend()
+            plt.show()
+            plt.figure(2)
+            plt.plot(micrograph.r*np.pi, micrograph.approx_noise_psd, label='Approx Noise PSD')
+            plt.title('Approx Noise PSD stage I')
+            plt.legend()
+            plt.show()
+        micrograph.approx_noise_psd = micrograph.approx_noise_psd + np.median(micrograph.approx_noise_psd)/10
+        micrograph.prewhiten_micrograph()
+        print("Done prewhitening.\nEstimating RPSD II:")
+        micrograph.estimate_rpsd(kltpicker.patch_size, kltpicker.max_iter)
+        print("Done estimating RPSD II.\nConstructing KLT templates:")
+        if kltpicker.show_figures:
+            plt.figure(3)
+            plt.plot(micrograph.r*np.pi, micrograph.approx_clean_psd, label='Approx Clean PSD')
+            plt.title('Approx Clean PSD stage II')
+            plt.legend()
+            plt.show()
+            plt.figure(4)
+            plt.plot(micrograph.r*np.pi, micrograph.approx_noise_psd, label='Approx Noise PSD')
+            plt.title('Approx Noise PSD stage II')
+            plt.legend()
+            plt.show()
+        micrograph.psd = np.abs(trig_interpolation(np.pi * micrograph.r.astype('float64'), micrograph.approx_clean_psd, kltpicker.rho.astype('float64')))
+        if kltpicker.show_figures:
+            plt.figure(5)
+            plt.plot(kltpicker.rho, micrograph.psd)
+            plt.title('Clean Sig Samp at nodes max order %i, percent of eig %f'%(kltpicker.max_order, PERCENT_EIG_FUNC))
+            plt.show()
         get_matlab_files(micrograph)
         micrograph.construct_klt_templates(kltpicker)
-        print("Done construct klt templates")
-        print("Picking particles:")
+        print("Done constructing KLT templates.\nPicking particles:")
         micrograph.detect_particles(kltpicker)
     print("")
 
